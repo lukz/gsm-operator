6b04340f7d5120beed4357c011804b44a944c29b
Arek Rebacz <rarykos@gmail.com>
2018-01-27 23:03:42 +0100

    [*] Arek COLOURS

--- Assets/_SCRIPTS/GroundSpawner.cs
+++ Assets/_SCRIPTS/GroundSpawner.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class GroundSpawner : MonoBehaviour {
 
-	public GameObject tile;
+	public GameObject[] tiles;
 	
 	void Start () {
-		if (!tile) {
+		if (tiles.Length==0) {
 			Debug.LogError("Ground Tile is missing");
 			return;
 		}
 		Transform parent = GetComponent<Transform>();
-		for (int x = -5; x < 5; x++)
+		for (float x = -5; x < 5; x++)
 		{	
-			for (int y = -5; y < 5; y++)
+			for (float y = -5; y < 5; y++)
 			{
-				GameObject.Instantiate(tile, new Vector3(x, y, 0), Quaternion.identity, parent);
+				int which = Random.Range(0, tiles.Length);
+				GameObject ob =  GameObject.Instantiate(tiles[which], new Vector3(-0.6f+x, y, 0), Quaternion.identity, parent);
+				float val = 1 - ((Mathf.Abs(x) + Mathf.Abs(y))/10f * 0.3f) - Random.Range(0.02f,0.05f);
+
+
+					ob.GetComponent<SpriteRenderer>().color = new Color(val, val, val*1.3f);
+				
 			}
 		}
 	}
