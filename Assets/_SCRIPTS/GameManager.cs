cb5ec4a19b58dba5fff48babb46f2db2f944973c
Arek Rebacz <rarykos@gmail.com>
2018-01-27 14:24:15 +0100

    arek

--- Assets/_SCRIPTS/GameManager.cs
+++ Assets/_SCRIPTS/GameManager.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour   {
+
+	public static int currentTowerCircle = 3; 
+	public static int currentTowerArc = 3;
+	public static int currentTowerLine = 3;
 
 
+	[SerializeField]
+	private Text countTowerCircle;
+
+	[SerializeField]
+	private Text countTowerArc;
+
+	[SerializeField]
+	private Text countTowerLine;
+
+	
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+
 	// Update is called once per frame
 	void Update () {
 		
