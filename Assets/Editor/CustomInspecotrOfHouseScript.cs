ce7551c53d8aff78dac0ec49bd74f8904a74b051
Łukasz Żmudziak <l.zmudziak@gmail.com>
2018-01-27 18:33:22 +0100

    House spot

---
+++ Assets/Editor/CustomInspecotrOfHouseScript.cs
@@ -0,0 +1,19 @@
+﻿//c# Example (LookAtPointEditor.cs)
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(HouseSpot))]
+
+public class CustomInspecotrOfHouseScript : Editor
+{
+    
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Build Object"))
+        {
+            ((HouseSpot)target).SpawnTier(((HouseSpot)target).asdas);
+        }
+
+        DrawDefaultInspector();
+    }
+}
--- Assets/HouseSpot.cs
+++ Assets/HouseSpot.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class HouseSpot : MonoBehaviour {
 
+    public List<GameObject> houseTiers;
+
+    public int asdas;
+
 	// Use this for initialization
 	void Start () {
-		
+        //SpawnTier(0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void SpawnTier(int tier)
+    {
+        Clear();
+
+        if (tier >= houseTiers.Count) return;
+
+        if (houseTiers[tier] == null) return;
+
+        Instantiate(houseTiers[tier], new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity, transform); 
+    }
+
+    public void Clear()
+    {
+        HouseScript[] childreens = gameObject.GetComponentsInChildren<HouseScript>();
+
+        for (int i = 0; i < childreens.Length; i++)
+        {
+            GameObject.Destroy(childreens[i].gameObject);
+        }
+
+    }
+
 }
