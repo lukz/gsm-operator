f9bce8774f034847ddaa7c265c600d3bf2098223
Arek Rebacz <rarykos@gmail.com>
2018-01-27 15:02:32 +0100

    [+] Limity budowania

---
+++ Assets/_SCRIPTS/CheckAvailable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckAvailable : MonoBehaviour {
+
+	[SerializeField]
+	private int numberTower;
+
+	[SerializeField]
+	private GameObject tower;
+
+
+
+	[SerializeField]
+	private TowerSpawner towerspawner;
+
+	public void SetTower()
+	{
+		if (GameManager.currentTowers[numberTower] > 0)
+		{
+			GameManager.currentTowers[numberTower]--;
+			GameManager.UpdateNumbers();
+			towerspawner.spawn(tower);
+
+		}
+	}
+
+}
