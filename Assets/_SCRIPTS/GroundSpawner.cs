14321490ecea9bb479c22e31ad1c6166a2e6081b
Arek Rebacz <rarykos@gmail.com>
2018-01-27 23:35:16 +0100

    arek jest strasznym du[kiem

--- Assets/_SCRIPTS/GroundSpawner.cs
+++ Assets/_SCRIPTS/GroundSpawner.cs
@@ -17,8 +17,12 @@ public class GroundSpawner : MonoBehaviour {
 			for (float y = -5; y < 5; y++)
 			{
 				int which = Random.Range(0, tiles.Length);
-				GameObject ob =  GameObject.Instantiate(tiles[which], new Vector3(-0.6f+x, y, 0), Quaternion.identity, parent);
+				GameObject ob =  GameObject.Instantiate(tiles[which], new Vector3(-0.8f+x, y, 0), Quaternion.identity, parent);
 				float val = 1 - ((Mathf.Abs(x) + Mathf.Abs(y))/10f * 0.3f) - Random.Range(0.02f,0.05f);
+				if (Mathf.Abs(x) + Mathf.Abs(y) < 2)
+				{
+					val = 0.96f;
+				}
 
 
 					ob.GetComponent<SpriteRenderer>().color = new Color(val, val, val*1.3f);
