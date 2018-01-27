aae2ea3bad5a3549a265968f1c727791b3e043f2
piotrj <piotr@piotrjastrzebski.io>
2018-01-27 18:03:58 +0100

    added scenes

---
+++ Assets/_SCRIPTS/ChangeScene.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+	public void changeScene(string name) {
+		SceneManager.LoadScene(name);
+	}
+}
