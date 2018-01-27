5ff4691f76df807c59c856258c204de2939ae087
Łukasz Żmudziak <l.zmudziak@gmail.com>
2018-01-27 15:18:17 +0100

    Power script

---
+++ Assets/_SCRIPTS/PowerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerScript : MonoBehaviour
+{
+
+    private List<GameObject> powered;
+
+    // Use this for initialization
+    void Start()
+    {
+        powered = new List<GameObject>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!transform.parent.GetComponent<TowerScript>().isBuilded) return;
+        if (other.transform.parent.gameObject.tag != "House") return;
+
+        if (!powered.Contains(other.gameObject))
+        {
+            powered.Add(other.gameObject);
+
+            other.transform.parent.GetComponent<HouseScript>().powerUp();
+        }
+    }
+
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (!transform.parent.GetComponent<TowerScript>().isBuilded) return;
+        if (other.transform.parent.gameObject.tag != "House") return;
+
+        if (powered.Contains(other.gameObject))
+        {
+            powered.Remove(other.gameObject);
+
+            other.transform.parent.GetComponent<HouseScript>().powerDown();
+        }
+    }
+
+
+}
