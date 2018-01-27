9710010e8e54221f745daa58f69198db8e08575c
piotrj <piotr@piotrjastrzebski.io>
2018-01-27 14:09:37 +0100

    switch scene after all houses are powered up for a while

--- Assets/_SCRIPTS/GameManager.cs
+++ Assets/_SCRIPTS/GameManager.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
-
-
+	public float powerUpTimeForSceneChange = 2;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+	float timer;
 	// Update is called once per frame
 	void Update () {
-		
+		GameObject[] houses = GameObject.FindGameObjectsWithTag("House");
+		if (houses.Length > 0) {
+			bool allPowered = true;		
+			foreach (GameObject house in houses)
+			{
+				HouseScript hs = house.GetComponent<HouseScript>();
+				allPowered &= hs.Powered;
+			}
+
+			if (allPowered) {
+				timer += Time.deltaTime;
+				if (timer >= powerUpTimeForSceneChange) {
+					Debug.Log("Full power");
+					SceneManager.LoadScene("TEST");
+					timer = 0;
+				}
+			} else {
+				timer = 0;
+			}
+		}
 	}
 }
