1b08764a984f389fad97f44df5157a406bbbb969
piotrj <piotr@piotrjastrzebski.io>
2018-01-28 12:29:13 +0100

    sand particle effect

---
+++ Assets/_SCRIPTS/DestroyOnDone.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnDone : MonoBehaviour {
+	public void destroy() {
+		GameObject.Destroy(gameObject);
+	}
+}
