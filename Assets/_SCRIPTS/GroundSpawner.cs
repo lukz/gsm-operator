1e758a139922a56ca3dc9ec2ad3749fbf1424b2c
Arek Rebacz <rarykos@gmail.com>
2018-01-28 12:18:56 +0100

    zmiea

--- Assets/_SCRIPTS/GroundSpawner.cs
+++ Assets/_SCRIPTS/GroundSpawner.cs
@@ -12,13 +12,13 @@ public class GroundSpawner : MonoBehaviour {
 			return;
 		}
 		Transform parent = GetComponent<Transform>();
-		for (float x = -5; x < 5; x++)
+		for (float x = -6; x < 6; x++)
 		{	
-			for (float y = -5; y < 5; y++)
+			for (float y = -6; y < 6; y++)
 			{
 				int which = Random.Range(0, tiles.Length);
-				GameObject ob =  GameObject.Instantiate(tiles[which], new Vector3(-0.8f+x, y, 0), Quaternion.identity, parent);
-				float val = 1 - ((Mathf.Abs(x) + Mathf.Abs(y))/10f * 0.3f) - Random.Range(0.02f,0.05f);
+				GameObject ob =  GameObject.Instantiate(tiles[which], new Vector3(-0.8f+x*0.56f, y*0.56f, 0), Quaternion.identity, parent);
+				float val = 1 - ((Mathf.Abs(x) + Mathf.Abs(y))/12f * 0.3f) - Random.Range(0.02f,0.05f);
 				if (Mathf.Abs(x) + Mathf.Abs(y) < 2)
 				{
 					val = 0.96f;
