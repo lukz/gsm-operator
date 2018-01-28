b03b99c2e8a78b3a9c1ec4847a8ff0575cabb344
Arek Rebacz <rarykos@gmail.com>
2018-01-28 15:17:54 +0100

    hjbj

---
+++ Assets/_SCRIPTS/LVLsettings.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LVLsettings : MonoBehaviour {
+
+	public int tower1aCount = 0;
+	public int tower2aCount = 0;
+	public int tower3aCount = 0;
+	public int tower1bCount = 0;
+	public int tower2bCount = 0;
+	public int tower3bCount = 0;
+	public int tower1cCount = 0;
+	public int tower2cCount = 0;
+	public int tower3cCount = 0;
+
+
+
+	public string nextScene = "TEST";
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
