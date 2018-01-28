eca19110ac1b0f31beaacda38b3484ad597ca51c
piotrj <piotr@piotrjastrzebski.io>
2018-01-28 13:44:00 +0100

    rotate buttons

---
+++ Assets/_SCRIPTS/RotateIcon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateIcon : MonoBehaviour {
+	public GameObject icon;
+	public void rotateLeft() {
+		Vector3 angles = icon.transform.rotation.eulerAngles;
+		icon.transform.rotation = Quaternion.Euler(angles.x, angles.y, angles.z - 90);
+	}
+	public void rotateRght() {
+		Vector3 angles = icon.transform.rotation.eulerAngles;
+		icon.transform.rotation = Quaternion.Euler(angles.x, angles.y, angles.z + 90);
+	}
+}
