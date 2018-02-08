7e991f06cc0ffc6acd5504c5a07898b5003790ae
Arek Rebacz <rarykos@gmail.com>
2018-02-08 16:40:03 +0100

    [*] NEW MAIN

--- Assets/_SCRIPTS/CameraResize.cs
+++ Assets/_SCRIPTS/CameraResize.cs
@@ -7,12 +7,19 @@ public class CameraResize : MonoBehaviour {
 	float orthoSize = 3;
 	float width = 9;
 	float height = 6;
+
+	[SerializeField]
+	private Canvas guiCanvas;
 	
 	void Update () {
 		float targetAspect = width/height;
 		float aspect = Screen.width/(float)Screen.height;
 		
-		Camera.main.orthographicSize = Mathf.Max(orthoSize, orthoSize * targetAspect/aspect);		
+		Camera.main.orthographicSize = Mathf.Max(orthoSize, orthoSize * targetAspect/aspect);
 		// Camera.main.orthographicSize = orthoSize * targetAspect/aspect;		
+
+		//Vector3 newpos = new Vector3 ((Screen.width-900), (Screen.height/600)-1f, guiCanvas.transform.position.z);
+
+		//guiCanvas.transform.position = newpos;
 	}
 }
