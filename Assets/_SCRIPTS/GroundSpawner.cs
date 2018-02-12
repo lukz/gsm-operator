1bd066983b363f39e987c575336800b07c711ad1
Arek <rarykos@gmail.com>
2018-02-12 16:16:33 +0100

    [*] ground spawner urocza perspektywa

--- Assets/_SCRIPTS/GroundSpawner.cs
+++ Assets/_SCRIPTS/GroundSpawner.cs
@@ -13,7 +13,10 @@ public class GroundSpawner : MonoBehaviour {
 
 	[Range(0, 1)]
 	public float multiplierDark;
-	
+
+	[Range(0, 1)]
+	public float scaleVanishingPoint;
+
 
 	[FloatRange(.1f, 3)]
 	public FloatRange spawnFrequency;
@@ -41,25 +44,31 @@ public class GroundSpawner : MonoBehaviour {
 			// size of the single tile, ie pixel width/cam pixels per unit
 			const float size = 0.56f;
 			float hw = (width + size)/2;
-			float hh = (height + size)/2;
+			float hh = 1f*(height + size)/2;
 			for (float x = -hw; x <= hw; x += size)
-			{	
-				for (float y = -hh; y <= hh; y += size)
+			{
+				float sca = 1f;
+				for (float y = -hh; y <= hh; y += size*sca)
 				{
+					sca = Mathf.Max(0.15f,Mathf.Min(1,(1 - (y * scaleVanishingPoint))));
 					int which = Random.Range(0, tiles.Length);
 					GameObject go = tiles[which];
 					if (!go) {
 						Debug.LogError("Ground: Tile at " + which + " missing");
 						continue;
 					}
-					Vector3 pos = new Vector3(cx + x -1, cy + y, 0);
+					Vector3 pos = new Vector3(cx + x -1.1f, cy + y - size*(1-sca)+0.1f, 0);
+
 					GameObject ob =  GameObject.Instantiate(tiles[which], pos, Quaternion.identity, transform);
+
+					ob.transform.localScale = new Vector3(1,sca,1);
 					float val = 1 - ((Mathf.Abs(x) + Mathf.Abs(y))/12f * 0.4f) - Random.Range(0.02f,0.05f);
 					if (Mathf.Abs(x) + Mathf.Abs(y) < 2)
 					{
 						val = 1f;
 					}
 					val *= multiplierDark;
+					val -= Mathf.Abs(sca-1)*0.25f;
 
 					ob.GetComponent<SpriteRenderer>().color = new Color(val, val, val*1.3f);
 				}
@@ -81,12 +90,14 @@ public class GroundSpawner : MonoBehaviour {
 				} 
 				Vector3 pos = new Vector3(cx + Random.Range(-hw, hw)-1, cx + Random.Range(-hh, hh), 0);
 				go = GameObject.Instantiate(go, pos, Quaternion.identity, transform);
-
-				float val = 1 - ((Mathf.Abs(go.transform.position.x) + Mathf.Abs(go.transform.position.y)) / 12f * 0.2f) - Random.Range(0.02f, 0.05f);
+				float sca = Mathf.Max(0.45f,Mathf.Min(1, 1 - (go.transform.position.y / 5 * 0.9f)));
+				go.transform.localScale = new Vector3(1, sca, 1);
+				float val = 1 - ((Mathf.Abs(go.transform.position.x) + Mathf.Abs(go.transform.position.y)) / 12f * 0.12f) - Random.Range(0.02f, 0.05f);
 				if (Mathf.Abs(go.transform.position.x) + Mathf.Abs(go.transform.position.y) < 2)
 				{
 					val = 1f;
 				}
+				val -= (go.transform.position.y / 4 * 0.1f);
 				go.GetComponent<SpriteRenderer>().color = new Color(val, val, val * 1.2f);
 
 			}
