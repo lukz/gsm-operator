a000e034146fee4f4d5855cfd89bd7a9c1ac66b5
Łukasz Żmudziak <l.zmudziak@gmail.com>
2018-03-06 18:25:43 +0100

    Lost file

---
+++ Assets/_SCRIPTS/YearSplashScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class YearSplashScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void showSplash(int year)
+    {
+        SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        SuperTextMesh textMesh = GetComponentInChildren<SuperTextMesh>();
+
+        textMesh.Text = "YEAR   " + year;
+
+        DOTween.Sequence()
+            .SetDelay(1f)
+            .Append(
+                spriteRenderer.DOFade(0.3f, 2f)
+            ).OnComplete(() => {
+                //gameObject.SetActive(false);
+                Destroy(gameObject);
+            });
+    }
+}
