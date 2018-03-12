598acc0f6589444e43bb6dfa65fc7586631c60e9
Arek Rebacz <rarykos@gmail.com>
2018-03-12 12:08:04 +0100

    fix

--- Assets/_SCRIPTS/GameManager.cs
+++ Assets/_SCRIPTS/GameManager.cs
@@ -141,7 +141,6 @@ public class GameManager : MonoBehaviour
 
 			GameObject newSplash = Instantiate(splash);
 			newSplash.GetComponent<YearSplashScript>().ShowSplash(lvlmanager.levelName);
-			newSplash.GetComponent<YearSplashScript>().reDraw() ;
 
 			UnlockNextTower();
 
