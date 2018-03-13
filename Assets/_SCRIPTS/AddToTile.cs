df450ee48887b81bd65c2785e994428daf8c99b3
piotrj <piotr@piotrjastrzebski.io>
2018-03-13 12:46:40 +0100

    fixed android compilation error, removed main from load order

--- Assets/_SCRIPTS/AddToTile.cs
+++ Assets/_SCRIPTS/AddToTile.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
-
+#endif
 
 [ExecuteInEditMode]
 public class AddToTile : MonoBehaviour {
