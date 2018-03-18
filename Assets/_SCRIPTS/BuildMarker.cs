fffd642c0e693900ad345f8d68cd3a66af595142
piotrj <piotr@piotrjastrzebski.io>
2018-03-18 17:22:24 +0100

    will power, new markers

---
+++ Assets/_SCRIPTS/BuildMarker.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildMarker : MonoBehaviour {
+	const int CAN_BUILD_OFF = 0;
+	const int CAN_BUILD_ALLOW = 1;
+	const int CAN_BUILD_DISALLOW = 2;
+
+	public GameObject willPowerMarker;
+	private Animator canBuildAnimator;
+	void Init () {
+		if (canBuildAnimator == null) {
+			canBuildAnimator = GetComponent<Animator>();
+		}
+	}
+
+	public void StartBuild(bool canBuild) {
+		Init();
+		if (canBuild) {
+			canBuildAnimator.SetInteger("state", CAN_BUILD_ALLOW);
+		} else {
+			canBuildAnimator.SetInteger("state", CAN_BUILD_DISALLOW);
+		}
+	}
+
+	public void CancelBuild() {
+		Init();
+		canBuildAnimator.SetInteger("state", CAN_BUILD_OFF);
+	}
+
+	public void StartWillPowerUp() {
+		willPowerMarker.SetActive(true);
+	}
+
+	public void CancelWillPowerUp() {
+		willPowerMarker.SetActive(false);
+	}
+}
--- Assets/_SCRIPTS/Tileset.cs
+++ Assets/_SCRIPTS/Tileset.cs
@@ -6,23 +6,59 @@ public class Tileset : MonoBehaviour {
 
     public List<TileRow> tiles;
 
-    void Start () {
+    private const int MAP_HEIGHT = 4;
+    private const int MAP_WIDTH = 5;
+    // x, y
+    private Tile[,] map = new Tile[MAP_WIDTH, MAP_HEIGHT];
+    float tileSize = 1.125f;
 
+    void Start () {
+        // figure out where each tile is on a grid
+        
+		foreach (var row in tiles) {
+            foreach (var tile in row.row) {
+                Vector3 tp = tile.transform.position;
+                Tile t = tile.GetComponent<Tile>();
+                
+                int x = GridX(tp.x);
+                int y = GridY(tp.y);
+                // Debug.Log("Tile " + t);
+                // Debug.Log(tp + " -> [" + x +", " + y + "]");
+                map[x, y] = t;
+            }
+		}
 	}
+
+    public int GridX (float x) 
+    {
+        return Mathf.FloorToInt((3.8f + x)/tileSize);
+    }
+
+    public int GridY (float y) 
+    {
+        // return MAP_HEIGHT - 1 - Mathf.FloorToInt((2.3f +y)/tileSize);
+        return Mathf.FloorToInt((2.3f +y)/tileSize);
+    }
 	
 	Tile lastTile;
 	void Update () {
 		if (false) {
 			Vector3 mp = InputUtils.WorldMousePosition();
 			Tile tile = GetTileAt(mp);
+            Debug.Log("Cursor at x= " + mp.x + ", y= " +mp.y);
+            Debug.Log("Cursor at ox= " + (3.8f + mp.x) + ", oy= " + (2.3f + mp.y));
+            Debug.Log("Cursor at sx= " + (3.8f + mp.x)/tileSize + ", soy= " + (2.3f + mp.y)/tileSize);
+            Debug.Log("Cursor at gx= " + GridX(mp.x) + ", gy= " + GridY(mp.y));
 			if (tile != lastTile) {
 				if (lastTile != null) {
 					SpriteRenderer oldSprite = lastTile.gameObject.GetComponent<SpriteRenderer>();
 					oldSprite.color = new Color(1, 1, 1, .5f);
 				}
 				lastTile = tile;
-				SpriteRenderer sprite = tile.gameObject.GetComponent<SpriteRenderer>();
-				sprite.color = new Color(1, 1, 1, 1);
+                if (tile != null) {
+				    SpriteRenderer sprite = tile.gameObject.GetComponent<SpriteRenderer>();
+				    sprite.color = new Color(1, 1, 1, 1);
+                }
 				Debug.Log("Tile at " + mp + ": " + tile);
 			}
 		}
@@ -64,6 +100,7 @@ public class Tileset : MonoBehaviour {
     }
 
 	public Tile GetTileAt(Vector3 at) {
+        // return GetTileAt(GridX(at.x), GridY(at.y));
 		Rect tmp = new Rect();
 		// tiles are 108 pixels, at 100 p/u, but theres a small gap
 		float tileSize = 1.15f;
@@ -80,6 +117,28 @@ public class Tileset : MonoBehaviour {
 		}
 		return null;
 	}
+
+    public Tile GetTileAt(int x, int y) {
+        if (x < 0 || x >= MAP_WIDTH || y < 0 || y >= MAP_HEIGHT) return null;
+        // return map[x, MAP_HEIGHT - 1 - y];
+        return map[x, y];
+    }
+
+    public void ShowTileBuildStatus() {
+        foreach (var row in tiles) {
+            foreach (var tile in row.row) {
+                tile.GetComponent<Tile>().ShowBuildStatus();
+            }
+		}
+    }
+
+    public void HideTileBuildStatus() {
+        foreach (var row in tiles) {
+            foreach (var tile in row.row) {
+                tile.GetComponent<Tile>().HideBuildStatus();
+            }
+		}
+    }
 
     [System.Serializable]
     public class TileRow
